d079d39ebbfb47604c004872a18a5fec768cadc5
이원빈 <leewonbeen@iwonbins-MacBook-Pro.local>
2023-05-22 13:34:07 +0900

    test commit

---
+++ testrepo/testrepo/testrepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  testrepoApp.swift
+//  testrepo
+//
+//  Created by 이원빈 on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct testrepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
